6feac3d648fc06410b12695bfd6fc0bf58513765
Luis Loza <lloza@usc.edu>
2018-03-16 12:35:08 -0700

    Update RegisterServlet.java

--- src/main/java/codeu/controller/RegisterServlet.java
+++ src/main/java/codeu/controller/RegisterServlet.java
@@ -1,14 +1,9 @@
 package codeu.controller;
-
 import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import codeu.model.store.basic.UserStore;
-import codeu.model.data.User;
-import java.time.Instant;
-import java.util.UUID;
 
 /**
 * Servlet class responsible for user registration.
@@ -19,46 +14,6 @@ public class RegisterServlet extends HttpServlet {
  public void doGet(HttpServletRequest request, HttpServletResponse response)
      throws IOException, ServletException {
 
-   request.getRequestDispatcher("/WEB-INF/view/register.jsp").forward(request, response);
+   response.getWriter().println("<h1>RegisterServlet GET request.</h1>");
  }
-
-    @Override
-    public void doPost(HttpServletRequest request, HttpServletResponse response)
-        throws IOException, ServletException {
-
-    String username = request.getParameter("username");
-    String password = request.getParameter("password");
-
-    if (!username.matches("[\\w*\\s*]*")) {
-        request.setAttribute("error", "Please enter only letters, numbers, and spaces.");
-        request.getRequestDispatcher("/WEB-INF/view/register.jsp").forward(request, response);
-        return;
-    }
-
-    if (userStore.isUserRegistered(username)) {
-        request.setAttribute("error", "That username is already taken.");
-        request.getRequestDispatcher("/WEB-INF/view/register.jsp").forward(request, response);
-        return;
-    }
-
-    User user = new User(UUID.randomUUID(), username, password, Instant.now());
-    userStore.addUser(user);
-
-    response.sendRedirect("/login");
-    }
-    private UserStore userStore;
-    @Override
-    public void init() throws ServletException {
-        super.init();
-        setUserStore(UserStore.getInstance());
-    }
-
-    /**
-    * Sets the UserStore used by this servlet. This function provides a common setup method
-    * for use by the test framework or the servlet's init() function.
-    */
-    void setUserStore(UserStore userStore) {
-        this.userStore = userStore;
-    }
-
 }
