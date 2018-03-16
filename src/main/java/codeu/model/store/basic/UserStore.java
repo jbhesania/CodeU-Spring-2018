637dfb0969b01ce3d300fd9fcd8a9ceb16e36773
Luis Loza <Luis@Luiss-MBP-3.attlocal.net>
2018-03-15 19:08:40 -0700

    Update for Part 3

--- src/main/java/codeu/model/store/basic/UserStore.java
+++ src/main/java/codeu/model/store/basic/UserStore.java
@@ -122,4 +122,28 @@ public class UserStore {
     this.users = users;
   }
   
+  @Override
+  public void doPost(HttpServletRequest request, HttpServletResponse response)
+      throws IOException, ServletException {
+
+    String username = request.getParameter("username");
+    String password = request.getParameter("password");
+
+    if (!username.matches("[\\w*\\s*]*")) {
+      request.setAttribute("error", "Please enter only letters, numbers, and spaces.");
+      request.getRequestDispatcher("/WEB-INF/view/register.jsp").forward(request, response);
+      return;
+    }
+
+    if (userStore.isUserRegistered(username)) {
+      request.setAttribute("error", "That username is already taken.");
+      request.getRequestDispatcher("/WEB-INF/view/register.jsp").forward(request, response);
+      return;
+    }
+
+    User user = new User(UUID.randomUUID(), username, password, Instant.now());
+    userStore.addUser(user);
+
+    response.sendRedirect("/login");
+  }
 }
