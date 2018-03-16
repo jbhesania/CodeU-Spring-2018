efd7293db19ac7fe86c06731dd625a0e4297a9ef
Luis Loza <Luis@Luiss-MBP-3.attlocal.net>
2018-03-15 19:10:15 -0700

    Update for Part 3

--- src/main/java/codeu/controller/RegisterServlet.java
+++ src/main/java/codeu/controller/RegisterServlet.java
@@ -10,50 +10,28 @@ import javax.servlet.http.HttpServletResponse;
 * Servlet class responsible for user registration.
 */
 public class RegisterServlet extends HttpServlet {
-    /**
-    * Store class that gives access to Users.
-    */
-    private UserStore userStore;
-    
-    /**
-    * Set up state for handling registration-related requests. This method is only called when
-    * running in a server, not when running in a test.
-    */
-    @Override
-    public void init() throws ServletException {
-        super.init();
-        setUserStore(UserStore.getInstance());
-    }
-    
-    /**
-    * Sets the UserStore used by this servlet. This function provides a common setup method
-    * for use by the test framework or the servlet's init() function.
-    */
-    void setUserStore(UserStore userStore) {
-        this.userStore = userStore;
-    }
 
     @Override
     public void doGet(HttpServletRequest request, HttpServletResponse response)
         throws IOException, ServletException {
 
-        request.getRequestDispatcher("/WEB-INF/view/register.jsp").forward(request, response);
+    request.getRequestDispatcher("/WEB-INF/view/register.jsp").forward(request, response);
     }
 
     @Override
     public void doPost(HttpServletRequest request, HttpServletResponse response)
         throws IOException, ServletException {
 
-        String username = request.getParameter("username");
-        String password = request.getParameter("password");
+    String username = request.getParameter("username");
+    String password = request.getParameter("password");
 
-        if (!username.matches("[\\w*\\s*]*")) {
-            request.setAttribute("error", "Please enter only letters, numbers, and spaces.");
-            request.getRequestDispatcher("/WEB-INF/view/register.jsp").forward(request, response);
-            return;
-        }   
+    if (!username.matches("[\\w*\\s*]*")) {
+        request.setAttribute("error", "Please enter only letters, numbers, and spaces.");
+        request.getRequestDispatcher("/WEB-INF/view/register.jsp").forward(request, response);
+        return;
+    }
 
-        response.getWriter().println("<p>Username: " + username + "</p>");
-        response.getWriter().println("<p>Password: " + password + "</p>");
+    response.getWriter().println("<p>Username: " + username + "</p>");
+    response.getWriter().println("<p>Password: " + password + "</p>");
     }
 }
