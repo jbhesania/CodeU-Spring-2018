da4f66d9bfaec306e08af1e290527c2086d39621
Luis Loza <Luis@Luiss-MBP-3.attlocal.net>
2018-03-15 18:43:29 -0700

    Update for Part 3

--- src/main/java/codeu/controller/RegisterServlet.java
+++ src/main/java/codeu/controller/RegisterServlet.java
@@ -15,7 +15,7 @@ import javax.servlet.http.HttpServletResponse;
         public void doGet(HttpServletRequest request, HttpServletResponse response)
                 throws IOException, ServletException {
 
-            response.getWriter().println("<h1>RegisterServlet GET request.</h1>");
+            request.getRequestDispatcher("/WEB-INF/view/register.jsp").forward(request, response);
         }
     }
 
