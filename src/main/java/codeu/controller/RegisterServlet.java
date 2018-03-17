c51360abdc7c0a13409046f22ba4a07e39444547
Luis Loza <lloza@usc.edu>
2018-03-16 21:34:46 -0700

    Update RegisterServlet.java

--- src/main/java/codeu/controller/RegisterServlet.java
+++ src/main/java/codeu/controller/RegisterServlet.java
@@ -14,51 +14,53 @@ import java.util.UUID;
 * Servlet class responsible for user registration.
 */
 public class RegisterServlet extends HttpServlet {
-
- @Override
- public void doGet(HttpServletRequest request, HttpServletResponse response)
-     throws IOException, ServletException {
-
-   request.getRequestDispatcher("/WEB-INF/view/register.jsp").forward(request, response);
- }
-
-    @Override
-    public void doPost(HttpServletRequest request, HttpServletResponse response)
-        throws IOException, ServletException {
-
-    String username = request.getParameter("username");
-    String password = request.getParameter("password");
-
-    if (!username.matches("[\\w*\\s*]*")) {
-        request.setAttribute("error", "Please enter only letters, numbers, and spaces.");
-        request.getRequestDispatcher("/WEB-INF/view/register.jsp").forward(request, response);
-        return;
-    }
-
-    if (userStore.isUserRegistered(username)) {
-        request.setAttribute("error", "That username is already taken.");
-        request.getRequestDispatcher("/WEB-INF/view/register.jsp").forward(request, response);
-        return;
-    }
-
-    User user = new User(UUID.randomUUID(), username, password, Instant.now());
-    userStore.addUser(user);
-
-    response.sendRedirect("/login");
-    }
+    
     private UserStore userStore;
+ 
     @Override
     public void init() throws ServletException {
         super.init();
         setUserStore(UserStore.getInstance());
     }
 
+    @Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
+        throws IOException, ServletException {
+
+
+        request.getRequestDispatcher("/WEB-INF/view/register.jsp").forward(request, response);
+    }
+
+    @Override
+    public void doPost(HttpServletRequest request, HttpServletResponse response)
+        throws IOException, ServletException {
+
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+
+        if (!username.matches("[\\w*\\s*]*")) {
+            request.setAttribute("error", "Please enter only letters, numbers, and spaces.");
+            request.getRequestDispatcher("/WEB-INF/view/register.jsp").forward(request, response);
+            return;
+        }
+
+        if (userStore.isUserRegistered(username)) {
+        request.setAttribute("error", "That username is already taken.");
+        request.getRequestDispatcher("/WEB-INF/view/register.jsp").forward(request, response);
+        return;
+        }
+
+        User user = new User(UUID.randomUUID(), username, password, Instant.now());
+        userStore.addUser(user);
+        response.sendRedirect("/login");
+    
+    }
+
     /**
-    * Sets the UserStore used by this servlet. This function provides a common setup method
-    * for use by the test framework or the servlet's init() function.
-    */
+     * Sets the UserStore used by this servlet. This function provides a common setup method
+     * for use by the test framework or the servlet's init() function.
+     */
     void setUserStore(UserStore userStore) {
         this.userStore = userStore;
     }
-
 }
